46280bd5dc8d78d641ea491b773432549f49be74
nafnu <21520@student.dorset-college.ie>
2020-12-16 11:55:28 +0000

    commit

---
+++ lab1.js
@@ -0,0 +1,27 @@
+var firstName = 'Nathalie';
+var favouriteColor = 'Turquoise'; 
+console.log(typeof favouriteColor);
+
+let favouriteFood = 'rice and beans';
+city  = 'Valdivia';
+
+console.log (firstName);  // Output: Nathalie
+console.log (favouriteColor);
+console.log (favouriteFood);
+
+let phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+let endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+const one = 'Red';
+const two = 'orange';
+const three = 'yellow';
+const four = 'green';
+const five = 'blue';
+const six = 'indigo';
+const seven = 'violet';
+
+console.log(phrase + one + ',' + two + ',' + three + ',' + four + ',' + five + ',' + six + ',' + seven + endingPhrase);
+
+/*
+console.log(`A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all. ${one}, ${two}, ${three}, ${four}, ${five}, ${six}, ${seven} is what we see. Rainbow colours, are made with light bending you see!’ );
+*/
+
